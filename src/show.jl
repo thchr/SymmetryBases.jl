a99c56d06b87f91fedf0e542a175020ed59381de
Thomas Christensen <thomas@dtu.dk>
2026-03-17 21:07:47 +0100

    bump Crystalline to v0.6.24 and PrettyTables to v3

--- src/show.jl
+++ src/show.jl
@@ -6,29 +6,32 @@ function show(io::IO, ::MIME"text/plain", sb::SymBasis)
                 length(sb), " Hilbert vectors, sampling ",
                 Nⁱʳʳ, " LGIrreps ",
                 "(spin-", isspinful(sb) ? "½" : "1", " ",
-                sb.timereversal ? "w/" : "w/o", " TR)")
+                sb.timereversal ? "w/" : "w/o", " TR):")
 
     k_idx = (i) -> findfirst(==(klabel(irreplabels(sb)[i])), klabels(sb)) # highlighters
-    h_odd = Highlighter((data,i,j) -> i≤Nⁱʳʳ && isodd(k_idx(i)), crayon"light_blue")
-    h_ν   = Highlighter((data,i,j) -> i==Nⁱʳʳ+1,                 crayon"light_yellow")
+    h_odd = TextHighlighter((data,i,j) -> i≤Nⁱʳʳ && isodd(k_idx(i)), crayon"light_blue")
+    h_ν   = TextHighlighter((data,i,j) -> i==Nⁱʳʳ+1,                 crayon"light_yellow")
 
     pretty_table(io, 
         # table contents
         stack(sb);
         # column/row names
-        header = eachindex(sb),
+        column_labels = eachindex(sb),
         row_labels = vcat(sb.irlabs, "μ"),
         # options/formatting/styling
-        formatters = (v,i,j) -> iszero(v) ? "·" : string(v),
-        vlines = [1,],
-        hlines = [:begin, 1, Nⁱʳʳ+1, :end],
-        row_label_alignment = :l,
+        formatters = [(v,i,j) -> iszero(v) ? "·" : string(v)],
+        table_format = TextTableFormat(;
+            horizontal_line_at_beginning = true,
+            horizontal_line_after_column_labels = true,
+            horizontal_line_after_data_rows = true,
+            horizontal_lines_at_data_rows = [Nⁱʳʳ],
+            @text__no_vertical_lines(),
+            vertical_line_after_row_label_column = true
+        ),
+        row_label_column_alignment = :l,
         alignment = :c, 
-        highlighters = (h_odd, h_ν), 
-        header_crayon = crayon"bold"
-        )
-
-    # print k-vec labels
-    print(io, "  KVecs: ")
-    join(io, klabels(sb), ", ")
+        highlighters = [h_odd, h_ν], 
+        style = TextTableStyle(column_label = crayon"bold"),
+        new_line_at_end = false
+    )
 end
